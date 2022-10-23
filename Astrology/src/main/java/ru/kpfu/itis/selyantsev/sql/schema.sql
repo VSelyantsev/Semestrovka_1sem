856272703a919a3f74989a5f5d5f9a967f00cca4
VSelyantsev <vselyantsev@mail.ru>
2022-10-23 17:16:48 +0300

    fixed sql schema

--- Astrology/src/main/java/ru/kpfu/itis/selyantsev/sql/schema.sql
+++ Astrology/src/main/java/ru/kpfu/itis/selyantsev/sql/schema.sql
@@ -4,8 +4,8 @@ create table account (
     id serial primary key,
     first_name varchar(255) not null,
     last_name varchar(255) not null,
-    login_name varchar(255) not null,
+    login_name varchar(255) unique not null,
     password_account varchar(255) not null,
     email varchar(255) not null,
     gender varchar(255) not null
-)
+);
