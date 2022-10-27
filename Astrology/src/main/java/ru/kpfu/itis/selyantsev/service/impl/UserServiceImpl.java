97e0df84778633a80be136e709f1f78c0bb627f8
VSelyantsev <vselyantsev@mail.ru>
2022-10-28 01:43:45 +0300

    added method search and realization

--- Astrology/src/main/java/ru/kpfu/itis/selyantsev/service/impl/UserServiceImpl.java
+++ Astrology/src/main/java/ru/kpfu/itis/selyantsev/service/impl/UserServiceImpl.java
@@ -1,6 +1,12 @@
 package ru.kpfu.itis.selyantsev.service.impl;
 
+import ru.kpfu.itis.selyantsev.dao.PlanetDAO;
+import ru.kpfu.itis.selyantsev.dao.ZodiacSignsDAO;
+import ru.kpfu.itis.selyantsev.dao.impl.PlanetDaoImpl;
+import ru.kpfu.itis.selyantsev.dao.impl.ZodiacSignsImpl;
+import ru.kpfu.itis.selyantsev.models.Planet;
 import ru.kpfu.itis.selyantsev.models.User;
+import ru.kpfu.itis.selyantsev.models.ZodiacSigns;
 import ru.kpfu.itis.selyantsev.util.PasswordUtil;
 
 import ru.kpfu.itis.selyantsev.dao.DAO;
@@ -12,6 +18,10 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 public class UserServiceImpl implements UserService {
+
+    private final PlanetDAO planetDAO = new PlanetDaoImpl();
+
+    private final ZodiacSignsDAO zodiacSignsDAO = new ZodiacSignsImpl();
 
     private final DAO userDao = new UserDaoImpl();
 
@@ -46,6 +56,31 @@ public class UserServiceImpl implements UserService {
     public boolean login(String loginName, String password) {
         User user = userDao.findByLoginName(loginName);
         return user.getPassword().equals(PasswordUtil.encrypt(password));
+    }
+
+    @Override
+    public String search(String request) {
+        String requestCondition = " ";
+
+        List<Planet> resultPlanetList = planetDAO.findAll()
+                .stream()
+                .filter(planet -> planet.getPlanetName().equals(request))
+                .collect(Collectors.toList());
+
+        List<ZodiacSigns> resultZodiacList = zodiacSignsDAO.findAll()
+                .stream()
+                .filter(constellation -> constellation.getZodiacName().equals(request))
+                .collect(Collectors.toList());
+
+        if (!resultPlanetList.isEmpty()) {
+            requestCondition = "planets.ftl";
+        } else if (!resultZodiacList.isEmpty()) {
+            requestCondition = "zodiacSigns.ftl";
+        } else {
+            requestCondition = "mainPage.ftl";
+        }
+
+        return requestCondition;
     }
 
 }
