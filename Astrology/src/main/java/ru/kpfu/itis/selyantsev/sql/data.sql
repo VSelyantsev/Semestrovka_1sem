5516cd3e27da9a67f0d1cd72f7f2215f6cc84061
VSelyantsev <vselyantsev@mail.ru>
2022-10-28 01:34:35 +0300

    filling tables

---
+++ Astrology/src/main/java/ru/kpfu/itis/selyantsev/sql/data.sql
@@ -0,0 +1,23 @@
+insert into planets(planet_name, planet_detection, planet_distance)
+    values ('Mercury', 'unknown', '58 million km'),
+           ('Venus', '1610', '108 million km'),
+           ('Earth', 'always observable', '150 million km'),
+           ('Mars', '1610', '228 million km'),
+           ('Jupiter', '1610', '778 million km'),
+           ('Saturn', '1655', '1429 million km'),
+           ('Uranus', '1781', '2875 million km'),
+           ('Neptune', '1846', '5913 million km');
+
+insert into zodiac_signs(zodiac_name, zodiac_type, zodiac_element)
+    values ('Aries', 'Northern', 'fire'),
+           ('Taurus', 'Northern', 'ground'),
+           ('Gemini', 'Northern°', 'air'),
+           ('Cancer', 'Northern', 'water'),
+           ('Leo', 'Northern', 'fire'),
+           ('Virgo', 'Northern', 'ground'),
+           ('Libra', 'Southern', 'air'),
+           ('Scorpius', 'Southern', 'water'),
+           ('Sagittarius', 'Southern', 'fire'),
+           ('Capricornus', 'Southern', 'ground'),
+           ('Aquarius', 'Southern', 'air'),
+           ('Pisces', 'Southern°', 'water');
