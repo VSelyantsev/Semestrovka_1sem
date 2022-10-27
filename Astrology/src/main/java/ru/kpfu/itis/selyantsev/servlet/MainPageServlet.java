b4d0aad83c0caee6dae7129bf01dcd04558c4ef1
VSelyantsev <vselyantsev@mail.ru>
2022-10-28 01:27:49 +0300

    added servlet for mainPage

--- Astrology/src/main/java/ru/kpfu/itis/selyantsev/servlet/MainPageServlet.java
+++ Astrology/src/main/java/ru/kpfu/itis/selyantsev/servlet/MainPageServlet.java
@@ -1,2 +1,33 @@
-package ru.kpfu.itis.selyantsev.servlet;public class MainPageServlet {
+package ru.kpfu.itis.selyantsev.servlet;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.kpfu.itis.selyantsev.service.UserService;
+import ru.kpfu.itis.selyantsev.service.impl.UserServiceImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "mainPage", urlPatterns = "/mainPage")
+public class MainPageServlet extends HttpServlet {
+
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MainPageServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.sendRedirect("mainPage.ftl");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        UserService userService = new UserServiceImpl();
+        String param = req.getParameter("request");
+        String resultSearch = userService.search(param);
+        resp.sendRedirect(resultSearch);
+    }
 }
