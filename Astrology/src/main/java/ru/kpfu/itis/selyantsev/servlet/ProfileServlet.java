5ee0afd4bb3b60dd040b78d8d5dc91270d15d5c5
VSelyantsev <vselyantsev@mail.ru>
2022-11-04 20:19:15 +0300

    modifed profile servlet

--- Astrology/src/main/java/ru/kpfu/itis/selyantsev/servlet/ProfileServlet.java
+++ Astrology/src/main/java/ru/kpfu/itis/selyantsev/servlet/ProfileServlet.java
@@ -1,12 +1,10 @@
 package ru.kpfu.itis.selyantsev.servlet;
 
-import ru.kpfu.itis.selyantsev.dao.ProfileDAO;
-import ru.kpfu.itis.selyantsev.dao.impl.ProfileDAOImpl;
-import ru.kpfu.itis.selyantsev.dto.UserDTO;
-import ru.kpfu.itis.selyantsev.models.User;
+import ru.kpfu.itis.selyantsev.service.ProfileService;
 import ru.kpfu.itis.selyantsev.service.UserService;
+import ru.kpfu.itis.selyantsev.service.impl.ProfileServiceImpl;
 import ru.kpfu.itis.selyantsev.service.impl.UserServiceImpl;
-import sun.java2d.cmm.Profile;
+import ru.kpfu.itis.selyantsev.models.Profile;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -20,13 +18,18 @@ import java.io.IOException;
 public class ProfileServlet extends HttpServlet {
 
     private final UserService userService = new UserServiceImpl();
+    private final ProfileService profileService = new ProfileServiceImpl();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession httpSession = req.getSession();
         String login = (String) httpSession.getAttribute("login");
+        String profilePhotoUrl = "http://res.cloudinary.com/duy2v66de/image/upload/v1667519879/chpstuk9kcit3kii4zbc.png";
+        String profileRole = "Not specified";
+        String profileHobby = "Not specified";
+        Profile profile = profileService.createProfile(login, profilePhotoUrl, profileRole, profileHobby);
         req.setAttribute("user", userService.findByLogin(login));
-        req.setAttribute("profile", userService.createNullProfile(login));
+        req.setAttribute("profile", profile);
         req.getRequestDispatcher("profile.ftl").forward(req, resp);
     }
 
