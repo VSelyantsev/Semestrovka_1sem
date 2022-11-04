49614f24b70f04e72760d55c93cc52268a172be7
VSelyantsev <vselyantsev@mail.ru>
2022-11-04 20:17:34 +0300

    added ProfileDao and implements it

--- Astrology/src/main/java/ru/kpfu/itis/selyantsev/service/ProfileService.java
+++ Astrology/src/main/java/ru/kpfu/itis/selyantsev/service/ProfileService.java
@@ -1,2 +1,11 @@
-package ru.kpfu.itis.selyantsev.service;public interface ProfileService {
+package ru.kpfu.itis.selyantsev.service;
+
+import ru.kpfu.itis.selyantsev.models.Profile;
+
+public interface ProfileService {
+
+    void delete(String loginName);
+    Profile createProfile(String loginName, String photoUrl, String profileRole, String profileHobby);
+
+    void save(Profile entity);
 }
--- Astrology/src/main/java/ru/kpfu/itis/selyantsev/service/impl/ProfileServiceImpl.java
+++ Astrology/src/main/java/ru/kpfu/itis/selyantsev/service/impl/ProfileServiceImpl.java
@@ -1,2 +1,41 @@
-package ru.kpfu.itis.selyantsev.service.impl;public class ProfileServiceImpl {
+package ru.kpfu.itis.selyantsev.service.impl;
+
+import ru.kpfu.itis.selyantsev.dao.DAO;
+import ru.kpfu.itis.selyantsev.dao.ProfileDAO;
+import ru.kpfu.itis.selyantsev.dao.impl.ProfileDAOImpl;
+import ru.kpfu.itis.selyantsev.dao.impl.UserDaoImpl;
+import ru.kpfu.itis.selyantsev.models.Profile;
+import ru.kpfu.itis.selyantsev.models.User;
+import ru.kpfu.itis.selyantsev.service.ProfileService;
+
+public class ProfileServiceImpl implements ProfileService {
+
+    private final ProfileDAO profileDAO = new ProfileDAOImpl();
+    private final DAO userDao = new UserDaoImpl();
+
+    @Override
+    public void delete(String loginName) {
+        profileDAO.delete(loginName);
+    }
+
+    @Override
+    public Profile createProfile(String loginName, String photoUrl,
+                                 String profileRole, String profileHobby ) {
+        User user = userDao.findByLoginName(loginName);
+        String accountLoginName = user.getLogin();
+        Profile profile = new Profile(
+                photoUrl,
+                profileRole,
+                profileHobby,
+                accountLoginName
+        );
+        return profile;
+    }
+
+    @Override
+    public void save(Profile entity) {
+        profileDAO.save(entity);
+    }
+
+
 }
