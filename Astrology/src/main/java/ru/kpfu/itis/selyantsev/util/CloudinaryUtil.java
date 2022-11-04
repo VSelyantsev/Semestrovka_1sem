b9299b6ea387d28e1e644949aed2a2db8f1c8119
VSelyantsev <vselyantsev@mail.ru>
2022-11-04 20:21:52 +0300

    added cloudinary util

--- Astrology/src/main/java/ru/kpfu/itis/selyantsev/util/CloudinaryUtil.java
+++ Astrology/src/main/java/ru/kpfu/itis/selyantsev/util/CloudinaryUtil.java
@@ -1,2 +1,22 @@
-package ru.kpfu.itis.selyantsev.util;public class CloudinaryUtil {
+package ru.kpfu.itis.selyantsev.util;
+
+import com.cloudinary.Cloudinary;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CloudinaryUtil {
+
+    private static Cloudinary cloudinary;
+
+    public static Cloudinary getInstance() {
+        if (cloudinary == null) {
+            Map configMap = new HashMap();
+            configMap.put("cloud_name", "duy2v66de");
+            configMap.put("api_key", "947547119335438");
+            configMap.put("api_secret", "Rc_ccGwWdfkBdFDcwXJWG7LD_58");
+            cloudinary = new Cloudinary(configMap);
+        }
+        return cloudinary;
+    }
 }
